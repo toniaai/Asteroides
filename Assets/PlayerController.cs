4e7b99f1fb632ab3ec2da408b8d01ebf24d29ee4
Antonio <antoni.aguirre@gmail.com>
2022-10-29 12:14:31 +0200

    Triangulo

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{   
+    [SerializeField] private Rigidbody2D rigidbody2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            
+        }
+
+
+        
+    }
+}
